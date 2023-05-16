3d17b9be8668d2b0e4f3e2941dc9309fbff7077d
amirvakili <95389620+amirhvakili@users.noreply.github.com>
2023-05-16 16:10:53 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("hi");
             Console.WriteLine("New_feature here...");
+            Console.WriteLine("Mama Miooo...");
         }
     }
 }
