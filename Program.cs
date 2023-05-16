77ec45f361e57c7436c08428fe5d11c48e7b45b2
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-16 14:04:07 +0330

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
+
+
