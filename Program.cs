c20ccd854a942266844323377a02321cc39435ae
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-16 20:29:36 +0330

    committed

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            var p47 = new P47();
-            Console.WriteLine("The total distance is {0} meters and {1} kilometers.", p47.TotalDistanceInMeters(), p47.TotalDistanceInMeters() / 1000);
+            
         }
     }
 }
