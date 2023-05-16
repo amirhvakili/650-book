5037501cddc6683e31743f00a80bad233e7ae3df
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-16 17:29:09 +0330

    committed

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
-            Console.WriteLine("New_feature here...");
-            Console.WriteLine("Mama Miooo...");
+
         }
     }
 }
