a4a7971bbacd59e225e68b913bcfdc10cf279cb3
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-16 14:13:20 +0330

    added new feature

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("hi");
+            Console.WriteLine("New_feature here...");
         }
     }
 }
