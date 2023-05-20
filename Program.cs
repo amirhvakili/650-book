8700fd3155379dea808827ff043c6b1d90e09587
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-20 16:17:42 +0330

    Fucker

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            
+            var p55 = new P55();
+            Console.WriteLine("The area of the triangle is: {0}", p55.S());
         }
     }
 }
