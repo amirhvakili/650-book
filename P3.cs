739e4191795fbe5b8a93b1b071740b64239a480d
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-25 14:05:16 +0330

    committed

---
+++ P3.cs
@@ -0,0 +1,45 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        public class P3
+        {
+            public P3()
+            {
+                int first = 1;
+                int second = 1;
+                int sum = 0;
+
+                Console.Write("Please enter N here: ");
+                var n = Convert.ToInt32(Console.ReadLine());
+
+                if(n == 1)
+                {
+                    Console.Write("The first N numbers of the fibunacci series are as follows: ");
+                    Console.WriteLine(first);
+                }
+                else if(n == 2)
+                {
+                    Console.Write("The first N numbers of the fibunacci series are as follows: ");
+                    Console.Write(first + " ");
+                    Console.WriteLine(second);
+                }
+                else
+                {
+                    Console.Write("The first N numbers of the fibunacci series are as follows: ");
+                    Console.Write(first + " ");
+                    Console.Write(second + " ");
+                    for (var i = 3; i <= n; i++)
+                    {
+                        sum = first + second;
+                        first = second;
+                        second = sum;
+                        Console.Write(sum + " ");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
