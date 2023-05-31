d5fe69156daf7b441606b99fc30f4884b417be10
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-05-31 18:36:55 +0330

    committed

---
+++ P12.cs
@@ -0,0 +1,38 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        public class P12
+        {
+            public P12()
+            {
+                
+                int x;
+                int y;
+                var result = 0;
+
+                do
+                {
+                    Console.Write("Enter your first number here: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    
+                    Console.Write("Enter your second number here: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+
+                    if (x == 0 && y == 0)
+                        break;
+
+                    for (var i = 1; i <= y; i++)
+                    {
+                        result += x;
+                    }
+
+                    Console.WriteLine("{0} multiplied by {1} equals = {2}", x, y, result);
+                }
+                while (true);
+            }
+        }
+    }
+}
+
+
