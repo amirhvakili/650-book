e6fe20e79f852697856bf1e570680f00fb372de0
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-06-01 17:13:09 +0330

    committed

---
+++ P15.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        public class P15
+        {
+            public P15()
+            {
+                Console.Write("How many numbers do you want to check: ");
+                var n = Convert.ToInt64(Console.ReadLine());
+
+                for(var i = 0; i < n; i++)
+                {
+                    Console.Write("Enter number: ");
+                    var number = Convert.ToInt64(Console.ReadLine());
+
+                    var temp = number;
+                    long sum = 0;
+
+                    while(temp > 0)
+                    {
+                        sum += temp % 10;
+                        temp /= 10;
+                    }
+
+                    if(sum % 9 == 0)
+                        Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
+
+
