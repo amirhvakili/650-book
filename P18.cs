45a05955de393a90b51bd1af3bf266db029a2dda
amirhosein7210@gmail.com <amirhosein7210@gmail.com>
2023-06-04 19:09:02 +0330

    committed

---
+++ P18.cs
@@ -0,0 +1,24 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        public class P18
+        {
+            public P18()
+            {
+                var counter = 7;
+
+                for(var i = 1; i <= 7; i++)
+                {
+                    for(var j = counter; j > 0; j--)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    counter--;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
